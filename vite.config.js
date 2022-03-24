f3e266e3816127d6d75c86ec6744ca8c199a5245
Mayank Jhawar <mayank_jhawar@certree.com>
2022-03-24 08:13:05 +0530

    Auto import component support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import { createVuePlugin } from 'vite-plugin-vue2';
-import visualizer from 'rollup-plugin-visualizer';
+import path from 'path'
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import Components from 'unplugin-vue-components/vite'
+
+import visualizer from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [createVuePlugin(), visualizer()],
+  plugins: [
+    createVuePlugin(),
+    visualizer(),
+    Components({
+      /* options */
+    }),
+  ],
   server: {
     port: 8080,
   },
@@ -20,4 +28,4 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 700, // Default is 500
   },
-});
+})
